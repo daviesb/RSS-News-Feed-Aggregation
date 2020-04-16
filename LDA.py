67cfee93c187d0d85d33b520f06b2e6b8496759d
daviesb <daviesb88@gmail.com>
2020-04-16 15:29:30 -0400

    added LDA function and modified main script

---
+++ LDA.py
@@ -0,0 +1,139 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Apr 14 00:15:55 2020
+
+@author: daviesb
+"""
+
+
+import pandas as pd
+import numpy as np
+import gensim
+import gensim.corpora as corpora
+from gensim.utils import simple_preprocess
+from nltk.corpus import stopwords
+
+
+def get_topics(df, num_topics):
+    
+    ### get stop words from NLTK
+    stop_words = stopwords.words('english')
+    stop_words.extend(['from', 'subject', 're', 'edu', 'use'])
+    
+    data = df.title.values.tolist()
+    # data_summary = df.summary.values.tolist()
+    # for summary in data_summary:
+    #     data.append(summary)
+    
+    ### tokenize words and clean up
+    def sent_to_words(sentences):
+        for sentence in sentences:
+            yield(gensim.utils.simple_preprocess(str(sentence), deacc=True)) # deacc=True removes punctuation
+            
+    data_words = list(sent_to_words(data))
+    
+    ### creating bigrams, trigrams, and more
+    bigram = gensim.models.Phrases(data_words, min_count=5, threshold=100) # higher threshold means fewer phrases
+    trigram = gensim.models.Phrases(bigram[data_words], threshold=100)
+    
+    ### faster way to get a sentence clubbed as a trigram/bigram
+    bigram_mod = gensim.models.phrases.Phraser(bigram)
+    trigram_mod = gensim.models.phrases.Phraser(trigram)
+    
+    
+    ### define functions for stopwords, bigrams, trigrams, and lemmatization
+    def remove_stopwords(texts):
+        return [[word for word in simple_preprocess(str(doc)) if word not in stop_words] for doc in texts]
+    
+    def make_bigrams(texts):
+        return [bigram_mod[doc] for doc in texts]
+    
+    def make_trigrams(texts):
+        return [trigram_mod[bigram_mod[doc]] for doc in texts]
+    
+    
+    ### calling functions
+    data_words_nostops = remove_stopwords(data_words)
+    data_words_bigrams = make_bigrams(data_words_nostops)
+    data_words_trigrams = make_trigrams(data_words_bigrams)
+    
+    
+    ### create dictionary and corpus
+    id2word = corpora.Dictionary(data_words_trigrams)
+    texts = data_words_trigrams
+    corpus = [id2word.doc2bow(text) for text in texts]
+    
+    # Human readable format of corpus (term-frequency)
+    # [[(id2word[id], freq) for id, freq in cp] for cp in corpus[:1]]
+        
+
+    ### build LDA model
+    # num_topics = 4
+    lda_model = gensim.models.ldamodel.LdaModel(
+        corpus = corpus,
+        id2word = id2word,
+        num_topics = num_topics,
+        random_state = 100,
+        update_every = 1,
+        chunksize = 100,
+        passes = 10,
+        alpha = 'auto',
+        per_word_topics = True)
+    
+    # pprint(lda_model.print_topics())
+   
+    
+    ### model perplexity and coherence
+    # print('Perplexity: ', lda_model.log_perplexity(corpus)) # a measure of how good the model is. lower is better.
+    
+    # coherence_model_lda = CoherenceModel(model=lda_model, texts=data_words_trigrams, dictionary= id2word, coherence='c_v')
+    # coherence_lda = coherence_model_lda.get_coherence()
+    # print('Coherence Score: ', coherence_lda)
+    
+    
+    ### take the messy output of top words in each of the topics and convert it to one data frame
+    df_topics = pd.DataFrame(columns=['Score', 'Word', 'Topic'])
+    for num_topic in range(num_topics):
+        df_temp = pd.DataFrame(lda_model.top_topics(corpus)[num_topic][0], columns=('Score', 'Word'))
+        df_temp['Topic'] = num_topic
+        df_topics = df_topics.append(df_temp, ignore_index=True)
+        
+        
+    ### this while loop extracts the top X words (one form each topic), based on num_topics
+    website_topics = []
+    while len(website_topics) < num_topics:
+        website_topics.append(df_topics['Word'][df_topics['Score'].argmax()]) # get highest score and return word
+        df_topics = df_topics[df_topics['Topic'] != df_topics['Topic'][df_topics['Score'].argmax()]] ### remove topic where word was taken from
+        df_topics = df_topics[df_topics['Word'].isin(website_topics) == False].reset_index(level=0, drop=True)
+    
+    ### title case and change underscore to space
+    website_topics = [item.title() for item in website_topics]
+    website_topics = [item.replace("_", " ") for item in website_topics]
+    ### print topics
+    print('Top ' + str(num_topics) + ' topics:\n')
+    for topic in website_topics:
+        print(topic)
+        
+    
+    
+    ### create empty dataframe to hold css_topic
+    css_topic = pd.DataFrame(index=range(0, len(df)), columns=['css_topic'])
+    css_topic = css_topic.replace(np.nan, '', regex=True)
+    ### generate JavaScript classes in css_topic
+    df.reset_index(drop=True, inplace=True)
+    for index, row in css_topic.iterrows():
+        for topic in range(num_topics):
+            if website_topics[topic].lower() in df['title'][index].lower():
+                css_topic['css_topic'][index] = css_topic['css_topic'][index] + "topic-" + str(topic) + " "
+                # print(str(index) + " contains " + str(website_topics[topic]))
+    
+    ### append css_topic to df
+    df = pd.concat([df, css_topic], axis=1)
+    
+
+    # ### append True/False for whether or not topic appears in each title (or summary if it's included earlier in script)
+    # for topic in range(num_topics):
+    #     df.insert(2, website_topics[topic], df['title'].str.lower().str.contains(website_topics[topic].lower()))
+        
+    return df, website_topics
--- viz_news_scrape.py
+++ viz_news_scrape.py
@@ -11,8 +11,9 @@ import pandas as pd
 import json
 import datetime
 import sys
-sys.path.append('/home/daviesb/Documents/viz.news-scripts/')
+sys.path.append('/home/daviesb/Documents/viz.news-scripts')
 from RSS import RSS
+import LDA
 #from FTP import FTP_Connect
         
 
@@ -49,6 +50,28 @@ rss_DailySig = RSS('Daily Signal', 'https://www.dailysignal.com/feed')
 rss_WashTimes = RSS('Washington Times', 'https://www.washingtontimes.com/rss/headlines/news/politics/')
 rss_Newsmax = RSS('Newsmax', 'https://www.newsmax.com/rss/Politics/1/')
 rss_Economist = RSS('The Economist', 'https://www.economist.com/united-states/rss.xml')
+rss_Blaze = RSS('The Blaze', 'https://www.theblaze.com/feeds/feed.rss')
+rss_ABC = RSS('ABC', 'https://abcnews.go.com/abcnews/topstories')
+rss_Townhall = RSS('Townhall', 'https://townhall.com/rss/columnists/')
+rss_BBC = RSS('BBC', 'http://feeds.bbci.co.uk/news/politics/rss.xml')
+rss_USAToday = RSS('USA Today', 'http://rssfeeds.usatoday.com/UsatodaycomWashington-TopStories')
+rss_Salon = RSS('Salon', 'https://www.salon.com/category/news-and-politics/feed')
+rss_Buzzfeed = RSS('BuzzFeed', 'https://www.buzzfeed.com/politics.xml')
+rss_CBS = RSS('CBS', 'https://www.cbsnews.com/latest/rss/politics')
+rss_Guardian = RSS('The Guardian', 'https://www.theguardian.com/us-news/us-politics/rss')
+rss_CBN = RSS('CBN', 'https://www1.cbn.com/rss-cbn-news-politics.xml')
+rss_NBC = RSS('NBC', 'http://feeds.nbcnews.com/nbcnews/public/politics')
+rss_FactCheck = RSS('FactCheck.org', 'https://www.factcheck.org/feed/')
+rss_Reason = RSS('Reason', 'https://reason.com/latest/feed/')
+rss_Atlantic = RSS('The Atlantic', 'https://www.theatlantic.com/feed/channel/politics/')
+rss_LA = RSS('Los Angeles Times', 'https://www.latimes.com/politics/rss2.0.xml#nt=1col-7030col1')
+rss_Forbes = RSS('Forbes', 'https://www.forbes.com/policy/feed/')
+rss_Federalist = RSS('The Federalist', 'https://thefederalist.com/feed/')
+rss_RCP = RSS('RealClearPolitics', 'https://www.realclearpolitics.com/index.xml')
+rss_538 = RSS('fivethirtyeight', 'https://fivethirtyeight.com/politics/feed/')
+rss_Heritage = RSS('The Heritage Foundation', 'https://www.heritage.org/rss')
+rss_RedState = RSS('RedState', 'https://www.redstate.com/feed/')
+
 #rss_BR = RSS('Bipartisan Report', 'https://bipartisanreport.com/feed/')
 #rss_Gateway = RSS('The Gateway Pundit', 'https://www.thegatewaypundit.com/feed/')
 #rss_Wonkette = RSS('Wonkette', 'https://www.wonkette.com/feeds/feed.rss')
@@ -60,7 +83,9 @@ rss_Economist = RSS('The Economist', 'https://www.economist.com/united-states/rs
 ### combine into one list and parse each feed
 rss_feed = [rss_NYT, rss_CNN, rss_Hill, rss_WSJ, rss_FOX, rss_Politico, rss_Alternet, rss_OAN, rss_AmSpec, rss_DailyBeast, rss_NYPost, \
             rss_NatReview, rss_HuffPo, rss_MoJo, rss_DailyKos, rss_WaPo, rss_NewYorker, rss_Slate, rss_Vox, rss_AmCon, rss_Axios, rss_NPR, \
-            rss_Breitbart, rss_CSM, rss_TPM, rss_Reuters, rss_WasMon, rss_WasExa, rss_DailySig, rss_WashTimes, rss_Newsmax]
+            rss_Breitbart, rss_CSM, rss_TPM, rss_Reuters, rss_WasMon, rss_WasExa, rss_DailySig, rss_WashTimes, rss_Newsmax, rss_Blaze, \
+            rss_ABC, rss_Townhall, rss_BBC, rss_USAToday, rss_Salon, rss_Buzzfeed, rss_CBS, rss_Guardian, rss_CBN, rss_NBC, rss_FactCheck, \
+            rss_Reason, rss_Atlantic, rss_LA, rss_Forbes, rss_Federalist, rss_RCP, rss_538, rss_Heritage, rss_RedState]
 df_rss = pd.DataFrame(columns=['source', 'title', 'summary', 'author', 'link', 'published'])
 for feed in rss_feed:
     feed.parse()
@@ -72,32 +97,65 @@ for feed in rss_feed:
     df_temp = pd.DataFrame({'source': feed.source, 'title':titles, 'link':links, 'published':published, 'author':authors, 'summary':summaries})
     df_rss = pd.concat([df_rss, df_temp], sort=False)
     
-### sort by published date (NOTE: need to flag time zone and articles with incorrect date)    
-df_rss['published_sort'] = pd.to_datetime(df_rss.published)
-df_rss.sort_values(['published_sort'], ascending=False, inplace=True)
+
+### These sources seem to have their published date in EST/EDT, while anything not listed is in GMT:
+time_adjust = [  
+"Huffington Post",
+"Vox",
+"Newsmax",
+"Christian Science Monitor",
+"NPR",
+"New York Post",
+"Reuters",
+"The Hill",
+"Wall Street Journal",
+"Washington Post",
+"Washington Times",
+"ABC",
+"Townhall",
+"CBS",
+"The Atlantic",
+"Los Angeles Times",
+"RealClearPolitics",
+"The Heritage Foundation",
+"RedState"
+]
+
+### sort by published date (NOTE: need to flag time zone and articles with incorrect date)
+df_rss['published_sort'] = pd.to_datetime(df_rss.published) # create datetime version of published date
 df_rss.reset_index(drop=True, inplace=True)
+df_rss = df_rss[df_rss['published_sort'].notnull()] # remove any records with no published date
+df_rss.loc[df_rss['source'].isin(time_adjust), 'published_sort'] = df_rss['published_sort'] + datetime.timedelta(hours=4) # convert EST publications to GMT
+df_rss.sort_values(['published_sort'], ascending=False, inplace=True)
+### this line briefly converts the string version of published to a datetime, adds 4 (or 5) hours to all the EST sources, and then converts back to string
+### it's used in JavaScript later, where the string is helpful (and needs to be in GMT)
+df_rss.loc[df_rss['source'].isin(time_adjust), 'published'] = (pd.to_datetime(df_rss['published']) + datetime.timedelta(hours=4)).apply(lambda x: datetime.datetime.strftime(x, '%b %d, %Y at %H:%M'))
+
 
 ### import bias and quality ratings from AdFontes Media
-df_bias = pd.read_csv('/home/daviesb/Documents/viz.news/sources/Overall-Source-Ratings-October-2019.csv', sep=',')
-df_bias = df_bias[df_bias.Quality >= 21]
+df_bias = pd.read_csv('/home/daviesb/Documents/viz.news/sources/bd_sources.csv')
+#df_bias = df_bias[df_bias.Quality >= 21]
 df_bias['bias_normalized'] = (df_bias.Bias - df_bias.Bias.mean()) / df_bias.Bias.std() # normalized bias scores
 df_bias['bias_minmax'] = (df_bias.Bias - df_bias.Bias.min()) / (df_bias.Bias.max() - df_bias.Bias.min()) # 0/1 min max scores
 
 ### scores below to be used for css settings on webpage. bias_css determins the x coordinate of where the article appears. quality_css determines the font size
-df_temp_left = df_bias[df_bias.Bias <= 0]
-left_min = 0
-left_max = 0.4
-df_temp_left['bias_css'] = (left_max - left_min) * (df_temp_left.Bias - df_temp_left.Bias.min()) / (left_max - df_temp_left.Bias.min()) + left_min
-
-df_temp_right = df_bias[df_bias.Bias > 0]
-right_min = 0.4
-right_max = 0.8
-df_temp_right['bias_css'] = (right_max - right_min) * (df_temp_right.Bias) / (df_temp_right.Bias.max()) + right_min
-
-df_bias = pd.merge(df_bias, df_temp_left[['source', 'bias_css']], on='source', how='left')
-df_bias = pd.merge(df_bias, df_temp_right[['source', 'bias_css']], on='source', how='left')
-df_bias.bias_css_x.update(df_bias.pop('bias_css_y'))
-df_bias.rename(columns={'bias_css_x':'bias_css'}, inplace=True)
+def create_css_bias(df):
+    if df.Bias == 3:
+        return 0.75
+    elif df.Bias == 2:
+        return 0.6333
+    elif df.Bias == 1:
+        return 0.5166
+    elif df.Bias == -3:
+        return 0.05
+    elif df.Bias == -2:
+        return 0.1666
+    elif df.Bias == -1:
+        return 0.2833
+    else: 
+        return .4
+    
+df_bias['bias_css'] = df_bias.apply(create_css_bias, axis=1)
 
 #scale_min = 0
 #scale_max = 0.8
@@ -106,33 +164,35 @@ scale_min_quality = 0.8
 scale_max_quality = 1.1
 df_bias['quality_css'] = (scale_max_quality - scale_min_quality) * (df_bias.Quality - df_bias.Quality.min()) / (df_bias.Quality.max() - df_bias.Quality.min()) + scale_min_quality
 
+
 ### join bias data with rss data
 df_rss = pd.merge(df_rss, df_bias, on='source', how='left')
 df_rss = df_rss[df_rss['published_sort'].notnull()] # remove articles with no published date
-two_days_ago = datetime.datetime.today() - datetime.timedelta(days=2) + datetime.timedelta(hours=5)
-one_day_ago = datetime.datetime.today() - datetime.timedelta(days=1) + datetime.timedelta(hours=5)
+two_days_ago = datetime.datetime.today() - datetime.timedelta(days=2) + datetime.timedelta(hours=4)
+one_day_ago = datetime.datetime.today() - datetime.timedelta(days=1) + datetime.timedelta(hours=4)
 df_rss = df_rss[df_rss['published_sort'] > one_day_ago] # filter out articles that are max 24 (or 48) hours old
 
 
 ### function to assign color values for modular css generation
 def create_css_fill(df):
-    if df.Bias >=19:
+    if df.Bias == 3:
         return 'background-image: linear-gradient(#BF2200, #BF2200);', 'mouseover-white', 'link-light' # dark red
-    elif df.Bias >= 10:
+    elif df.Bias == 2:
         return 'background-image: linear-gradient(#EC7063, #EC7063);', 'mouseover-black', 'link-light' # red
-    elif df.Bias >= 4:
+    elif df.Bias == 1:
         return 'background-image: linear-gradient(#F2D7D5, #F2D7D5);', 'mouseover-black', 'link-dark' # light red
-    elif df.Bias <= -19:
+    elif df.Bias == -3:
         return 'background-image: linear-gradient(#1C2ACD, #1C2ACD);', 'mouseover-white', 'link-light' # dark blue
-    elif df.Bias <= -10:
+    elif df.Bias == -2:
         return 'background-image: linear-gradient(#00BCFF, #00BCFF);', 'mouseover-black', 'link-light' # blue
-    elif df.Bias <= -4:
+    elif df.Bias == -1:
         return 'background-image: linear-gradient(#D6F4FF, #D6F4FF);', 'mouseover-black', 'link-dark' # light blue
     else: 
         return 'background-image: linear-gradient(#EEEEEE, #EEEEEE);', 'mouseover-black', 'link-dark' # slight gray - neutral
       
 ### apply function
 df_rss['css_fill'], df_rss['css_text'], df_rss['css_link_text'] = zip(*df_rss.apply(create_css_fill, axis=1))
+
 
 
 
@@ -152,6 +212,16 @@ df_rss.drop_duplicates(inplace=True)
 #df_rss.title[df_rss.source=='AP'] = df_rss.title[df_rss.source=='AP'].apply(lambda x: x.split('http', 1)[0])
 
 
+
+### topic modelling using LDA
+df_rss, topics = LDA.get_topics(df_rss, num_topics=4)
+
+
+### save topics to txt file
+with open('/home/daviesb/Documents/viz.news/txt/topics.txt', 'w') as text_file:
+    text_file.write(str(topics))
+
+### save json files
 json_rss = df_rss.to_json(orient='records')
 json_to_ftp = json.dumps(json_rss)
 
@@ -161,7 +231,8 @@ with open('/home/daviesb/Documents/viz.news/json/rss_records.json', 'w') as json
 df_rss.to_json('/home/daviesb/Documents/viz.news/json/rss.json')
 
 
-today = datetime.datetime.today() + datetime.timedelta(hours=5)
+
+today = datetime.datetime.today() + datetime.timedelta(hours=4)
 timestamp = today.strftime('Last updated at %H:%M GMT on %b %d, %Y')
 with open('/home/daviesb/Documents/viz.news/txt/timestamp.txt', 'w') as text_file:
     text_file.write(timestamp)
@@ -170,15 +241,17 @@ with open('/home/daviesb/Documents/viz.news/txt/timestamp.txt', 'w') as text_fil
 #FTP_Connect()
 
 
-## connecting to FTP and autouploading files
-from ftplib import FTP_TLS
-import ssl
-import time
+# ## connecting to FTP and autouploading files
+# from ftplib import FTP_TLS
+# import ssl
+# import time
 
-### FTP code here
+
 
 print('The average bias is ' + str(round(df_rss.Bias.mean(), 2)) + ', where negative is more liberal')
 print('{0:.0%}'.format(df_rss.Bias[df_rss.Bias < 0].count() / df_rss.Bias.count()) + ' of sources are left of center')
 
 
 
+
+### sql code
